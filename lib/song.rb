167b70fb0dd60e8483d9940e072e03c2ba99974f
Nathaniel Miller <dukeoflaser@gmail.com>
2016-02-19 02:59:11 +0000

    Rename variables

--- lib/song.rb
+++ lib/song.rb
@@ -31,26 +31,26 @@ class Song
     def genre_count
       genre_histogram = Hash.new
       
-      iterate_histogram(@@genres, genre_histogram)
+      iterate_to_histogram(@@genres, genre_histogram)
       genre_histogram
     end
     
     def artist_count
       artist_histogram = Hash.new
       
-      iterate_histogram(@@artists, artist_histogram)
+      iterate_to_histogram(@@artists, artist_histogram)
       artist_histogram
     end
     
-    def add_to_histogram(hash, key)
+    def histogram_maker(hash, key)
       hash[key] == nil ? hash[key] = 1 : hash[key] += 1
     end
     
-    def iterate_histogram(array, histogram)
-      array.each { |i| add_to_histogram(histogram, i) }
+    def iterate_to_histogram(array, hash)
+      array.each { |i| histogram_maker(hash, i) }
     end    
     
-    protected :add_to_histogram, :iterate_histogram
+    protected :histogram_maker, :iterate_to_histogram
   end
   
 end
